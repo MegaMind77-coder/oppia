2d7c4910ded16c1363d828d0c99203a293f11fb3
MegaMind77-coder <shanware.ishan@gmail.com>
2021-01-05 20:06:51 +0530

    reverted changes in question-backend-api.service.ts

--- core/templates/domain/question/question-backend-api.service.ts
+++ core/templates/domain/question/question-backend-api.service.ts
@@ -181,7 +181,7 @@ export class QuestionBackendApiService {
    * Returns a list of questions based on the list of skill ids and number
    * of questions requested.
    */
-  async fetchQuestionsAsync(
+  fetchQuestions(
       skillIds: string[], questionCount: number,
       questionsSortedByDifficulty: boolean): Promise<QuestionBackendDict[]> {
     return new Promise((resolve, reject) => {
@@ -191,13 +191,13 @@ export class QuestionBackendApiService {
     });
   }
 
-  async fetchTotalQuestionCountForSkillIdsAsync(skillIds: string[]): Promise<number> {
+  fetchTotalQuestionCountForSkillIds(skillIds: string[]): Promise<number> {
     return new Promise((resolve, reject) => {
       this._fetchTotalQuestionCountForSkillIds(skillIds, resolve, reject);
     });
   }
 
-  async fetchQuestionSummariesAsync(
+  fetchQuestionSummaries(
       skillId: string,
       cursor: string = ''): Promise<QuestionSummariesResponse> {
     return new Promise((resolve, reject) => {
